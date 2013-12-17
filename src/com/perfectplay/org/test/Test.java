6a6e40ed7b6febd26b391cf1b633f851f477de50
Hector Medina-Fetterman <javi@medina.net>
2013-12-17 01:45:58 -0500

    Update to file structure, update pom.xml

--- src/com/perfectplay/org/test/Test.java
+++ src/com/perfectplay/org/test/Test.java
@@ -1,10 +1,15 @@
 package com.perfectplay.org.test;
 
+import com.perfectplay.org.JLOL;
+
 public class Test {
 
 	public Test(){
-
+		JLOL.region = "na";
+		JLOL.api_key = "a1afbf2e-9eba-40e0-9aeb-6a8143404dde";
+		System.out.println(JLOL.getChampion("vi"));
 	}
+	
 	public static void main(String[] args){
 		new Test();
 	}
